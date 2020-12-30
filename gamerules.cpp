241d1e1a8873bfebc2ee73667e4c8dea837697b1
Xelonidas <mipokoj@gmail.com>
2020-12-30 13:14:20 +0100

    aktualizacja performMove()

--- gamerules.cpp
+++ gamerules.cpp
@@ -1,3 +1,24 @@
+void gameInit() {
+	return;
+}
+
+bool performMove(int xA, int yA, int xB, int yB) {
+	//nie można wykonać ruchu w miejscu
+	if(xA == xB && yA == yB) return false;
+	ChessPiece chessPiece = getChessPiece(xA, yA);
+	if(!chessPiece.moveFunctionPointer(int color, int xA, int yA, int xB, int yB)) {
+		return false;
+	}
+	removeChessPiece(xA, yA); removeChessPiece(yA, yB);
+	setChessPiece(chessPiece, xA, yA);
+	return true;
+}
+
+
+bool emptyTileMove(int color, int xA, int yA, int xB, int yB) {
+	return false;
+}
+
 bool checkPawnsMove(int color, int xA, int yA, int xB, int yB) {
 	return false;
 }
@@ -20,11 +41,6 @@ bool checkQueenMove(int color, int xA, int yA, int xB, int yB) {
 
 bool checkKingMove(int color, int xA, int yA, int xB, int yB) {
 	return false;
-}
-
-
-void performMove(int xA, int yA, int xB, int yB) {
-	return;
 }
 
 int checkWinCondition(int color) {
--- gamerules.h
+++ gamerules.h
@@ -1,6 +1,8 @@
+//Funkcja wczytuje domyślny stan planszy
+void gameInit();
 //Poniższe funkcje sprawdzają czy dany pionek może się poruszyć z pola A na pole B
-//Jeśli może to funkcja zwraca true i wykonuje ruch (performMove()) który aktualizuje stan planszy
-//w przeciwnym przypadku zwracany jest false
+//Jeśli może to funkcja zwraca true w przeciwnym przypadku zwracany jest false
+bool emptyTileMove(int color, int xA, int yA, int xB, int yB);
 bool checkPawnsMove(int color, int xA, int yA, int xB, int yB);
 bool checkBishopMove(int color, int xA, int yA, int xB, int yB);
 bool checkKnightMove(int color, int xA, int yA, int xB, int yB);
@@ -8,7 +10,10 @@ bool checkRookMove(int color, int xA, int yA, int xB, int yB);
 bool checkQueenMove(int color, int xA, int yA, int xB, int yB);
 bool checkKingMove(int color, int xA, int yA, int xB, int yB);
 
-void performMove(int xA, int yA, int xB, int yB);
+//performMove przyjmuje informacje o ruchy który trzeba wykonać.
+//sprawdzi za pomocą powyższych funkcji ckeckXYZMove(...) czy ruch może zostać wykonany
+//jeśli można go wykonać, to stan planszy zostanie zaktualizowany i zwrócony zostanie true
+bool performMove(int xA, int yA, int xB, int yB);
 
 //Funkcja sprawdza planszę i zwraca informacje o ewentualnym końcu gry
 // 0 - Nikt nie wygrał
